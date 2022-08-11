8b1632cee206cedb0316a25d96f1fabe90c6af35
Sachin Kumar <95373320+sachinkrr02@users.noreply.github.com>
2022-08-11 17:47:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+from unittest import result
+from flask import flask,jsonify, request
+import time
+
+
+app = flask(__name__)
+@app.route("/bot" , method=["POST"])
+
+
+def respone():
+    query = dict(request.form)['query']
+    result = query + " " + time.ctime()
+    return jsonify({"response" : result})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",)
